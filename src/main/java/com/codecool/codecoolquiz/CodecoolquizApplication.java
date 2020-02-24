a7a11a6d7c9c9ae7f1acfd0a969459f85cd1e752
Eszter Kobor <kobor.eszter@gmail.com>
2020-02-24 12:59:57 +0100

    initial commit

---
+++ src/main/java/com/codecool/codecoolquiz/CodecoolquizApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.codecoolquiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodecoolquizApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodecoolquizApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codecool/codecoolquiz/CodecoolquizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.codecoolquiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodecoolquizApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
