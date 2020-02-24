c28464d12745b49cc7f80610f41dd1b9fde99258
Eszter Kobor <kobor.eszter@gmail.com>
2020-02-24 13:33:49 +0100

    Add QuestionStorage

---
+++ src/main/java/com/codecool/codecoolquiz/service/QuestionStorage.java
@@ -0,0 +1,21 @@
+package com.codecool.codecoolquiz.service;
+
+import com.codecool.codecoolquiz.model.Question;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class QuestionStorage {
+
+    private List<Question> questions = new ArrayList<>();
+
+    public List<Question> getAll() {
+        return questions;
+    }
+
+    public void add(Question question) {
+        questions.add(question);
+    }
+}
