a9785a64ce3fa66be971dbd0e73ca07b3a202caf
Lorya <mircsi.lodi@gmail.com>
2020-02-25 11:50:05 +0100

    Implement getRandomQuestionsFromList method

---
+++ src/main/java/com/codecool/codecoolquiz/Util.java
@@ -0,0 +1,30 @@
+package com.codecool.codecoolquiz;
+
+import com.codecool.codecoolquiz.model.Question;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+@Component
+public class Util {
+
+    public List<Question> getRandomQuestionsFromList(List<Question> list, int numOfNeededItems) {
+        Random rand = new Random();
+
+        List<Question> copyList = new ArrayList<>(list);
+        List<Question> newList = new ArrayList<>();
+
+        for (int i = 0; i < numOfNeededItems; i++) {
+
+            int randomIndex = rand.nextInt(copyList.size());
+
+            newList.add(copyList.get(randomIndex));
+
+            copyList.remove(randomIndex);
+        }
+        return newList;
+    }
+
+}
