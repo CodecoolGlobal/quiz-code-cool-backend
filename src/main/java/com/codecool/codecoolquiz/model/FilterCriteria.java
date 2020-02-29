d7be90472500c9b8efafcd8084579a7b02825607
Lorya <mircsi.lodi@gmail.com>
2020-02-29 18:17:03 +0100

    Implement FilterCriteria class

---
+++ src/main/java/com/codecool/codecoolquiz/model/FilterCriteria.java
@@ -0,0 +1,22 @@
+package com.codecool.codecoolquiz.model;
+
+import javafx.util.Pair;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Component
+public class FilterCriteria {
+
+    private List<Pair<String, String>> filters;
+
+    @SafeVarargs
+    public final void setFilters(Pair<String, String>... parameters) {
+        this.filters = Arrays.asList(parameters);
+    }
+
+    public List<Pair<String, String>> getFilters() {
+        return filters;
+    }
+}
