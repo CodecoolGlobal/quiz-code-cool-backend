1e0d6120fcd8717fcdb5018acb676b8e4227582d
Lorya <mircsi.lodi@gmail.com>
2020-03-09 13:44:14 +0100

    Fix method name after merge

--- src/main/java/com/codecool/codecoolquiz/service/CustomQuizStorage.java
+++ src/main/java/com/codecool/codecoolquiz/service/CustomQuizStorage.java
@@ -33,6 +33,6 @@ public class CustomQuizStorage {
                 .stream()
                 .filter(customQuiz -> customQuiz.getId() == id)
                 .findFirst().orElse(null))
-                .getCustomQuizzes();
+                .getQuestions();
     }
 }
