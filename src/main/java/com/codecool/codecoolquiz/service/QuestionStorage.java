7e6660a07f4aba4c1aaeb66cf26fa5bd54732c21
Lorya <mircsi.lodi@gmail.com>
2020-03-10 18:46:42 +0100

    Refactor question controller to use SpecificationArgumentResolver

--- src/main/java/com/codecool/codecoolquiz/service/QuestionStorage.java
+++ src/main/java/com/codecool/codecoolquiz/service/QuestionStorage.java
@@ -1,29 +1,24 @@
 package com.codecool.codecoolquiz.service;
 
 import com.codecool.codecoolquiz.Util;
-import com.codecool.codecoolquiz.model.Category;
 import com.codecool.codecoolquiz.model.Question;
 import com.codecool.codecoolquiz.repository.QuestionRepository;
+import net.kaczmarzyk.spring.data.jpa.web.SpecificationArgumentResolver;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 @Service
-public class QuestionStorage {
-
-    @Autowired
-    Util util;
+public class QuestionStorage extends SpecificationArgumentResolver {
 
     @Autowired
     QuestionRepository questionRepository;
 
     @Autowired
     CategoryStorage categoryStorage;
-
-    private List<Question> questions = new ArrayList<>();
 
     public List<Question> getAll() {
         return questionRepository.findAll();
@@ -37,32 +32,7 @@ public class QuestionStorage {
         return questionRepository.findById(Integer.parseInt(questionId));
     }
 
-    public List<Question> getFilteredQuestions(String category, String type, String amount) {
-
-        List<Question> resultList;
-
-        if (category != null) {
-            Category questionCategory = categoryStorage.getById(Integer.parseInt(category));
-            if (type != null) {
-                resultList = questionRepository.findByCategoryAndType(questionCategory, type);
-            } else {
-                resultList = questionRepository.findByCategory(questionCategory);
-            }
-        } else if (type != null) {
-            resultList = questionRepository.findByType(type);
-        } else {
-            resultList = questionRepository.findAll();
-        }
-
-        if (amount == null) {
-            return resultList;
-        }
-
-        if (resultList.size() < Integer.parseInt(amount)) {
-            return null;
-        } else {
-            return util.getRandomQuestionsFromList(resultList, Integer.parseInt(amount));
-        }
-
+    public List<Question> findAll(Specification<Question> customerSpec) {
+        return questionRepository.findAll(customerSpec);
     }
 }
