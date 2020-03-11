10b9c6f72288a7371ffdcd59e6fb2b6722503743
MelathStofi <melath.stofi@gmail.com>
2020-03-11 10:52:31 +0100

    change configuration

--- src/main/java/com/codecool/codecoolquiz/CodecoolquizApplication.java
+++ src/main/java/com/codecool/codecoolquiz/CodecoolquizApplication.java
@@ -2,8 +2,10 @@ package com.codecool.codecoolquiz;
 
 import com.codecool.codecoolquiz.service.Initializer;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
 
 import javax.annotation.PostConstruct;
@@ -11,18 +13,10 @@ import javax.annotation.PostConstruct;
 @SpringBootApplication
 public class CodecoolquizApplication {
 
-    @Autowired
-    Initializer initializer;
-
     public static void main(String[] args) {
         SpringApplication.run(CodecoolquizApplication.class, args);
     }
 
-    @PostConstruct
-    @Profile("production")
-    public void afterInit() throws Exception {
-        initializer.loadInitData();
-    }
 
 
 }
--- src/test/java/com/codecool/codecoolquiz/AllRepositoryTest.java
+++ src/test/java/com/codecool/codecoolquiz/AllRepositoryTest.java
@@ -4,12 +4,14 @@ import com.codecool.codecoolquiz.model.CustomQuiz;
 import com.codecool.codecoolquiz.repository.CategoryRepository;
 import com.codecool.codecoolquiz.repository.CustomQuizRepository;
 import com.codecool.codecoolquiz.repository.QuestionRepository;
-import org.junit.Test;
+import com.codecool.codecoolquiz.service.Initializer;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -18,7 +20,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.util.List;
 
 @ExtendWith(SpringExtension.class)
-@SpringBootTest
 @DataJpaTest
 @ActiveProfiles("test")
 public class AllRepositoryTest {
