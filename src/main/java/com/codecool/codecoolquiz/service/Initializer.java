226a534a362f0deb4f3444ae4f62dbcfa40d44c0
Lorya <mircsi.lodi@gmail.com>
2020-03-12 20:23:15 +0100

    Fill database with data

--- src/main/java/com/codecool/codecoolquiz/service/Initializer.java
+++ src/main/java/com/codecool/codecoolquiz/service/Initializer.java
@@ -27,23 +27,28 @@ public class Initializer {
 
     @PostConstruct
     public void loadInitData() throws Exception {
-        loadInitCategories();
-        loadInitQuestions();
-        loadInitCustomQuizzes();
-    }
-
-    private void loadInitCategories() throws Exception {
         if (categoryStorage.categoryRepository.count() == 0) {
-            categoryStorage.add(Category.builder().name("Python").build());
-            categoryStorage.add(Category.builder().name("Java").build());
-            categoryStorage.add(Category.builder().name("CSS").build());
-            categoryStorage.add(Category.builder().name("SQL").build());
-            categoryStorage.add(Category.builder().name("HTML").build());
-            categoryStorage.add(Category.builder().name("General").build());
+            loadInitCategories();
+        }
+        if (questionStorage.questionRepository.count() == 0) {
+            loadInitQuestions();
+        }
+        if (customQuizStorage.customQuizRepository.count() == 0) {
+            loadInitCustomQuizzes();
         }
     }
 
-    private void loadInitQuestions() throws Exception {
+    private void loadInitCategories() {
+        categoryStorage.add(Category.builder().name("Python").build());
+        categoryStorage.add(Category.builder().name("Java").build());
+        categoryStorage.add(Category.builder().name("CSS").build());
+        categoryStorage.add(Category.builder().name("SQL").build());
+        categoryStorage.add(Category.builder().name("HTML").build());
+        categoryStorage.add(Category.builder().name("General").build());
+    }
+
+
+    private void loadInitQuestions() {
         Question question1 = Question.builder()
                 .category(categoryStorage.getById(1))
                 .type(Type.MULTIPLE)
@@ -84,150 +89,170 @@ public class Initializer {
                 .validationDate(null)
                 .isValidated(false)
                 .build();
-
-/*
-        Question question5 = new Question(
-                categoryStorage.getById(2),
-                "multiple",
-                "Which is a valid declarations of a String?",
-                "String s1 = null;",
-                Arrays.asList("String s2 = 'null';", "String s3 = (String) 'abc';", "String s4 = (String) '\\ufeed';")
-        );
-        Question question6 = new Question(
-                categoryStorage.getById(1),
-                "multiple",
-                "What is output for − 'search'. find('S') ?",
-                "interface",
-                Arrays.asList("s", "-1", "''")
-        );
-        Question question7 = new Question(
-                categoryStorage.getById(1),
-                "multiple",
-                "Which of the following is an invalid variable?",
-                "1st_string",
-                Arrays.asList("my_string_1", "foo", "_")
-        );
-        Question question8 = new Question(
-                categoryStorage.getById(1),
-                "multiple",
-                "Which of the following cannot be a variable?",
-                "in",
-                Arrays.asList("__init__", "it", "on")
-        );
-        Question question9 = new Question(
-                categoryStorage.getById(3),
-                "multiple",
-                "Which of the following property is used to change the face of a font?",
-                "font-family",
-                Arrays.asList("font-style", "font-variant", "font-weight")
-        );
-        Question question10 = new Question(
-                categoryStorage.getById(3),
-                "multiple",
-                "Which of the following property of a anchor element signifies visited hyperlinks?",
-                ":visited",
-                Arrays.asList(":link", ":hover", ":active")
-        );
-        Question question11 = new Question(
-                categoryStorage.getById(3),
-                "multiple",
-                "Which of the following selector selects an element that has no children?",
-                ":empty",
-                Arrays.asList(":nochild", ":inheritance", ":no-child")
-        );
-        Question question12 = new Question(
-                categoryStorage.getById(4),
-                "multiple",
-                "What is returned by TRUNC(789.8389, 2)?",
-                "789.83",
-                Arrays.asList("789.84", "78", "789.00")
-        );
-        Question question13 = new Question(
-                categoryStorage.getById(4),
-                "multiple",
-                "Which SQL function is used to count the number of rows in a SQL query?",
-                "COUNT(*)",
-                Arrays.asList("SUM()", "NUMBER()", "COUNT()")
-        );
-        Question question14 = new Question(
-                categoryStorage.getById(4),
-                "multiple",
-                "______ removes all rows from a table without logging the individual row deletions.",
-                "TRUNCATE",
-                Arrays.asList("DROP", "REMOVE", "DELETE")
-        );
-        Question question15 = new Question(
-                categoryStorage.getById(5),
-                "boolean",
-                "Choose the correct HTML element for the largest heading.",
-                "<h1>",
-                Arrays.asList("<h6>", "<head>", "<heading>")
-        );
-        Question question16 = new Question(
-                categoryStorage.getById(5),
-                "boolean",
-                "Choose the correct HTML element to define important text.",
-                "<strong>",
-                Arrays.asList("<b>", "<i>", "<important>")
-        );
-        Question question17 = new Question(
-                categoryStorage.getById(5),
-                "boolean",
-                "How can you make a numbered list?",
-                "<ol>",
-                Arrays.asList("<ul>", "<dl>", "<list>")
-        );*/
+        Question question5 = Question.builder()
+                .category(categoryStorage.getById(2))
+                .type(Type.MULTIPLE)
+                .question("Which is a valid declarations of a String?")
+                .correctAnswer("String s1 = null;")
+                .incorrectAnswers(Arrays.asList("String s2 = 'null';", "String s3 = (String) 'abc';", "String s4 = (String) '\\ufeed';"))
+                .creationDate(LocalDate.of(2020, 1, 3))
+                .validationDate(null)
+                .isValidated(false)
+                .build();
+        Question question6 = Question.builder()
+                .category(categoryStorage.getById(2))
+                .type(Type.MULTIPLE)
+                .question("What is output for − 'search'. find('S') ?")
+                .correctAnswer("interface")
+                .incorrectAnswers(Arrays.asList("s", "-1", "''"))
+                .creationDate(LocalDate.of(2020, 1, 5))
+                .validationDate(null)
+                .isValidated(false)
+                .build();
+        Question question7 = Question.builder()
+                .category(categoryStorage.getById(1))
+                .type(Type.MULTIPLE)
+                .question("Which of the following is an invalid variable?")
+                .correctAnswer("1st_string")
+                .incorrectAnswers(Arrays.asList("my_string_1", "foo", "_"))
+                .creationDate(LocalDate.of(2020, 1, 8))
+                .validationDate(null)
+                .isValidated(false)
+                .build();
+        Question question8 = Question.builder()
+                .category(categoryStorage.getById(1))
+                .type(Type.MULTIPLE)
+                .question("Which of the following cannot be a variable?")
+                .correctAnswer("in")
+                .incorrectAnswers(Arrays.asList("__init__", "it", "on"))
+                .creationDate(LocalDate.of(2020, 1, 10))
+                .validationDate(null)
+                .isValidated(false)
+                .build();
+        Question question9 = Question.builder()
+                .category(categoryStorage.getById(3))
+                .type(Type.MULTIPLE)
+                .question("Which of the following property is used to change the face of a font?")
+                .correctAnswer("font-family")
+                .incorrectAnswers(Arrays.asList("font-style", "font-variant", "font-weight"))
+                .creationDate(LocalDate.of(2020, 1, 15))
+                .validationDate(null)
+                .isValidated(false)
+                .build();
+        Question question10 = Question.builder()
+                .category(categoryStorage.getById(3))
+                .type(Type.MULTIPLE)
+                .question("Which of the following property of a anchor element signifies visited hyperlinks?")
+                .correctAnswer(":visited")
+                .incorrectAnswers(Arrays.asList(":link", ":hover", ":active"))
+                .creationDate(LocalDate.of(2020, 1, 19))
+                .validationDate(null)
+                .isValidated(false)
+                .build();
+        Question question11 = Question.builder()
+                .category(categoryStorage.getById(3))
+                .type(Type.MULTIPLE)
+                .question("Which of the following selector selects an element that has no children?")
+                .correctAnswer(":empty")
+                .incorrectAnswers(Arrays.asList(":nochild", ":inheritance", ":no-child"))
+                .creationDate(LocalDate.of(2020, 1, 19))
+                .validationDate(null)
+                .isValidated(true)
+                .build();
+        Question question12 = Question.builder()
+                .category(categoryStorage.getById(4))
+                .type(Type.MULTIPLE)
+                .question("What is returned by TRUNC(789.8389, 2)?")
+                .correctAnswer("789.83")
+                .incorrectAnswers(Arrays.asList("789.84", "78", "789.00"))
+                .creationDate(LocalDate.of(2020, 1, 27))
+                .validationDate(null)
+                .isValidated(true)
+                .build();
+        Question question13 = Question.builder()
+                .category(categoryStorage.getById(4))
+                .type(Type.MULTIPLE)
+                .question("Which SQL function is used to count the number of rows in a SQL query?")
+                .correctAnswer("COUNT(*)")
+                .incorrectAnswers(Arrays.asList("SUM()", "NUMBER()", "COUNT()"))
+                .creationDate(LocalDate.of(2020, 2, 4))
+                .validationDate(null)
+                .isValidated(true)
+                .build();
+        Question question14 = Question.builder()
+                .category(categoryStorage.getById(4))
+                .type(Type.MULTIPLE)
+                .question("______ removes all rows from a table without logging the individual row deletions.")
+                .correctAnswer("TRUNCATE")
+                .incorrectAnswers(Arrays.asList("DROP", "REMOVE", "DELETE"))
+                .creationDate(LocalDate.of(2020, 2, 9))
+                .validationDate(null)
+                .isValidated(true)
+                .build();
+        Question question15 = Question.builder()
+                .category(categoryStorage.getById(5))
+                .type(Type.MULTIPLE)
+                .question("Choose the correct HTML element for the largest heading.")
+                .correctAnswer("<h1>")
+                .incorrectAnswers(Arrays.asList("<h6>", "<head>", "<heading>"))
+                .creationDate(LocalDate.of(2020, 2, 13))
+                .validationDate(null)
+                .isValidated(true)
+                .build();
+        Question question16 = Question.builder()
+                .category(categoryStorage.getById(5))
+                .type(Type.MULTIPLE)
+                .question("Choose the correct HTML element to define important text.")
+                .correctAnswer("<strong>")
+                .incorrectAnswers(Arrays.asList("<b>", "<i>", "<important>"))
+                .creationDate(LocalDate.of(2020, 2, 20))
+                .validationDate(null)
+                .isValidated(true)
+                .build();
+        Question question17 = Question.builder()
+                .category(categoryStorage.getById(5))
+                .type(Type.MULTIPLE)
+                .question("How can you make a numbered list?")
+                .correctAnswer("<ol>")
+                .incorrectAnswers(Arrays.asList("<ul>", "<dl>", "<list>"))
+                .creationDate(LocalDate.of(2020, 2, 20))
+                .validationDate(null)
+                .isValidated(true)
+                .build();
         questionStorage.add(question1);
         questionStorage.add(question2);
         questionStorage.add(question3);
         questionStorage.add(question4);
-//        questionStorage.add(question5);
-//        questionStorage.add(question6);
-//        questionStorage.add(question7);
-//        questionStorage.add(question8);
-//        questionStorage.add(question9);
-//        questionStorage.add(question10);
-//        questionStorage.add(question11);
-//        questionStorage.add(question12);
-//        questionStorage.add(question13);
-//        questionStorage.add(question14);
-//        questionStorage.add(question15);
-//        questionStorage.add(question16);
-//        questionStorage.add(question17);
+        questionStorage.add(question5);
+        questionStorage.add(question6);
+        questionStorage.add(question7);
+        questionStorage.add(question8);
+        questionStorage.add(question9);
+        questionStorage.add(question10);
+        questionStorage.add(question11);
+        questionStorage.add(question12);
+        questionStorage.add(question13);
+        questionStorage.add(question14);
+        questionStorage.add(question15);
+        questionStorage.add(question16);
+        questionStorage.add(question17);
     }
 
     private void loadInitCustomQuizzes() {
         CustomQuiz quiz1 = CustomQuiz.builder().name("Eszti").build();
         CustomQuiz quiz2 = CustomQuiz.builder().name("Mira").build();
+        CustomQuiz quiz3 = CustomQuiz.builder().name("Droszi").build();
+        CustomQuiz quiz4 = CustomQuiz.builder().name("Stofi").build();
 
-        quiz1.setQuestions(questionStorage.getAll());
-        quiz2.setQuestions(questionStorage.getAll());
-        if (customQuizStorage.customQuizRepository.count() == 0) {
-            customQuizStorage.add(quiz1);
-            customQuizStorage.add(quiz2);
-        }
+        quiz1.setQuestions(questionStorage.getAll().subList(0, 5));
+        quiz2.setQuestions(questionStorage.getAll().subList(5, 10));
+        quiz3.setQuestions(questionStorage.getAll().subList(10, 15));
+        quiz4.setQuestions(questionStorage.getAll().subList(12, 16));
 
-/*        CustomQuiz customQuiz1 = new CustomQuiz(1, "Eszti");
-        CustomQuiz customQuiz2 = new CustomQuiz(2, "Mira");
-        CustomQuiz customQuiz3 = new CustomQuiz(3, "Droszi");
-        CustomQuiz customQuiz4 = new CustomQuiz(4, "Stofi");
-        for(int i = 0; i < questionStorage.getAll().size(); i++) {
-            if (i < 4) {
-                customQuiz1.add(questionStorage.getAll().get(i));
-            }
-            if (i >= 4 && i < 8) {
-                customQuiz2.add(questionStorage.getAll().get(i));
-            }
-            if (i >= 8 && i < 12) {
-                customQuiz3.add(questionStorage.getAll().get(i));
-            }
-            if (i >= 12 && i < questionStorage.getAll().size()) {
-                customQuiz4.add(questionStorage.getAll().get(i));
-            }
-        }
-        customQuizStorage.add(customQuiz1);
-        customQuizStorage.add(customQuiz2);
-        customQuizStorage.add(customQuiz3);
-        customQuizStorage.add(customQuiz4);*/
+        customQuizStorage.add(quiz1);
+        customQuizStorage.add(quiz2);
+        customQuizStorage.add(quiz3);
+        customQuizStorage.add(quiz4);
+
     }
 }
