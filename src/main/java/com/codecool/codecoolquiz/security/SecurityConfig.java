5e157655731b76d0fe9269eeeae260383320bb61
MelathStofi <melath.stofi@gmail.com>
2020-03-23 16:09:05 +0100

    Add token verification

--- src/main/java/com/codecool/codecoolquiz/security/SecurityConfig.java
+++ src/main/java/com/codecool/codecoolquiz/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.codecool.codecoolquiz.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -7,9 +8,13 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private JwtTokenServices jwtTokenServices;
 
     @Bean
     @Override
@@ -33,6 +38,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers(HttpMethod.POST,"/questions/**").authenticated() // allowed only when signed in
                 .antMatchers(HttpMethod.PUT,"/questions/**").hasRole("ADMIN") // allowed only when signed in
                 .antMatchers("/types/**").authenticated() // allowed only when signed in
-                .anyRequest().denyAll(); // anything else is denied
+                .anyRequest().denyAll() // anything else is denied
+            // NEW PART:
+            .and()
+            .addFilterBefore(new JwtTokenFilter(jwtTokenServices), UsernamePasswordAuthenticationFilter.class);
     }
 }
