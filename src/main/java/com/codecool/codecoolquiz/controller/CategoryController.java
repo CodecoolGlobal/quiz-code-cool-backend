d07deebc17584f66c2a4fb8f24774e04896a3bf9
Eszter Kobor <kobor.eszter@gmail.com>
2020-03-27 08:36:17 +0100

    Delete unused codes

--- src/main/java/com/codecool/codecoolquiz/controller/CategoryController.java
+++ src/main/java/com/codecool/codecoolquiz/controller/CategoryController.java
@@ -4,11 +4,8 @@ import com.codecool.codecoolquiz.model.Category;
 import com.codecool.codecoolquiz.service.CategoryStorage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @RestController
--- src/main/java/com/codecool/codecoolquiz/security/JwtTokenServices.java
+++ src/main/java/com/codecool/codecoolquiz/security/JwtTokenServices.java
@@ -9,11 +9,9 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
-import javax.servlet.http.HttpServletRequest;
 import java.security.Key;
 import java.util.*;
 import java.util.stream.Collectors;
@@ -48,33 +46,6 @@ public class JwtTokenServices {
                 .setExpiration(new Date(System.currentTimeMillis() + validityInMilliseconds))
                 .signWith(secretKey)
                 .compact();
-    }
-
-    // checks if the token is valid and not expired.
-    boolean validateToken(String token) {
-        try {
-            Jws<Claims> claims = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token);
-            return !claims.getBody().getExpiration().before(new Date());
-        } catch (JwtException | IllegalArgumentException e) {
-            log.debug("JWT token invalid " + e);
-        }
-        return false;
-    }
-    /**
-     * Parses the username and roles from the token. Since the token is signed we can be sure its valid information.
-     * Note that it does not make a DB call to be super fast!
-     * This could result in returning false data (e.g. the user was deleted, but their token has not expired yet)
-     * To prevent errors because of this make sure to check the user in the database for more important calls!
-     */
-    Authentication parseUserFromTokenInfo(String token) throws UsernameNotFoundException {
-        Claims body = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody();
-        String username = body.getSubject();
-        List<String> roles = (List<String>) body.get(rolesFieldName);
-        List<SimpleGrantedAuthority> authorities = new LinkedList<>();
-        for (String role : roles) {
-            authorities.add(new SimpleGrantedAuthority(role));
-        }
-        return new UsernamePasswordAuthenticationToken(username, "", authorities);
     }
 
     public UsernamePasswordAuthenticationToken validateTokenAndExtractUserSpringToken(String token) {
