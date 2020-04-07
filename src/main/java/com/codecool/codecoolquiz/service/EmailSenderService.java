3af2b5a4dbd2beb94ff3d167031d33521b101974
Lorya <mircsi.lodi@gmail.com>
2020-04-07 11:05:10 +0200

    Add logging

--- src/main/java/com/codecool/codecoolquiz/service/EmailSenderService.java
+++ src/main/java/com/codecool/codecoolquiz/service/EmailSenderService.java
@@ -1,11 +1,14 @@
 package com.codecool.codecoolquiz.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class EmailSenderService {
 
     @Autowired
@@ -19,6 +22,12 @@ public class EmailSenderService {
         msg.setSubject("Welcome " + username + "!");
         msg.setText("Thank you for registering on our site. Enjoy the quizzes! :)\n\nThe quiz.code.cool Team");
 
-        javaMailSender.send(msg);
+        try {
+            javaMailSender.send(msg);
+            log.info("E-mail was sent to: " + email + "!");
+
+        } catch (MailException e) {
+            log.error("E-mail couldn't be sent to: " + email + "!\n" + e.getMessage());
+        }
     }
 }
