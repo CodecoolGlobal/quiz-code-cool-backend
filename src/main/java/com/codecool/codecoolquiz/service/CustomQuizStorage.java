47718ad289e2347d0e907fa2f52e1ff3bac757a6
Drosztmér István <drosztmer.istvan@gmail.com>
2020-04-08 14:50:54 +0200

    Implement quizbody

--- src/main/java/com/codecool/codecoolquiz/service/CustomQuizStorage.java
+++ src/main/java/com/codecool/codecoolquiz/service/CustomQuizStorage.java
@@ -2,10 +2,12 @@ package com.codecool.codecoolquiz.service;
 
 import com.codecool.codecoolquiz.model.CustomQuiz;
 import com.codecool.codecoolquiz.model.Question;
+import com.codecool.codecoolquiz.model.QuizBody;
 import com.codecool.codecoolquiz.repository.CustomQuizRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Service
@@ -20,6 +22,16 @@ public class CustomQuizStorage {
 
     public void add(CustomQuiz customQuiz) {
         customQuizRepository.save(customQuiz);
+    }
+
+    public void addQuizBody(QuizBody quizBody) {
+        String name = quizBody.getName();
+        int[] questionIds = quizBody.getQuestionIds();
+//        Arrays.stream(questionIds).map(e -> )
+        CustomQuiz customQuiz = CustomQuiz.builder()
+                .name(name)
+//                .questions()
+                .build();
     }
 
     public List<Question> getQuestionsForCustomQuizById(int id) {
