f2df48895bbb91dfab727d7428a67e5a40423d2a
Eszter Kobor <kobor.eszter@gmail.com>
2020-06-14 01:13:47 +0200

    Change error message

--- src/main/java/com/codecool/codecoolquiz/model/exception/CustomQuizNameAlreadyExistException.java
+++ src/main/java/com/codecool/codecoolquiz/model/exception/CustomQuizNameAlreadyExistException.java
@@ -2,6 +2,6 @@ package com.codecool.codecoolquiz.model.exception;
 
 public class CustomQuizNameAlreadyExistException extends RuntimeException {
     public CustomQuizNameAlreadyExistException(String name) {
-        super("Custom quiz cannot be saved. Custom quiz already exists with the name '" + name + "'.");
+        super("Custom quiz cannot be saved. It already exists with the name '" + name + "'.");
     }
 }
